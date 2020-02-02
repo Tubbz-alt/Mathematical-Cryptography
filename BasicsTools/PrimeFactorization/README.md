bc6100e862d25319feb71ef3a909a61c50173306
Ali Rajabzadeh <alirajabzadeh.devacc@gmail.com>
2020-02-02 05:36:34 +0330

    updated

--- BasicsTools/PrimeFactorization/README.md
+++
@@ -1,7 +0,0 @@
-# Prime Factorization
-In the Mathematical Cryptography one of the most Important theorem is that every positive integer has an essentially unique factorization as a product of primes.
->![](theorem1.21.JPG)
-*from An introduction to the mathematical cryptography (joseph H.silverman) Book*
-### Implementation
-We need to implement theorem for use its application in the **Mathematical cryptography** , so we need to find efficient algortihm for finding the prime fators of integers number.
-one of the efficient way to finding prime factor for big integer use the **nextProbablePrime()** method in the **BigInteger class** of  java , and you can see in the PrimeFactors class in this repo. 
---
+++ BasicsTools/PrimitveRootSearch.java
@@ -0,0 +1,22 @@
+
+import PrimeFactorization.PrimeFactors;
+import java.math.*;
+import java.util.*;
+public class PrimitveRootSearch {
+	public static Boolean checkPrimitive(BigInteger number, Set<BigInteger> factors
+		BigInteger phi, BigInteger prime) {
+		for (BigInteger i : factors)
+			if (number.modPow(phi.divide(i), prime).equals(BigInteger.ONE))
+				return false;
+		return true; 
+	}
+	public static BigInteger primitiveRoot(BigInteger prime) {
+		BigInteger phi = prime.subtract(BigInteger.ONE);
+		Set<BigInteger> primeFactors = PrimeFactors.primeFactors(phi);
+		BigInteger generator = BigInteger.ONE;
+		do {
+			generator = generator.add(BigInteger.ONE);
+		} while (!checkPrimitive(generator, primeFactors, phi, prime));
+		return generator;
+	}
+}
